96a2e282e9f997f3a1cd7171aba68fd58625c9c3
Geethkiran <geeth53xy@gmail.com>
2020-04-01 09:32:46 +0530

    [geeth]Add. pojo class of StateCensusCSV is created

---
+++ com/censusanalyser/java/CSVStateCensus.java
@@ -0,0 +1,58 @@
+package CensusAnalyserProject.com.censusanalyser.java;
+
+import com.opencsv.bean.CsvBindByName;
+
+public class CSVStateCensus {
+
+    @CsvBindByName(column = "State")
+    private String stateName;
+
+    @CsvBindByName(column = "Population", required = true)
+    private String population;
+
+    @CsvBindByName(column = "AreaInSqKm")
+    private String areaInSqKm;
+
+    @CsvBindByName(column = "DensityPerSqKm", required = true)
+    private String densityPerSqKm;
+
+    public CSVStateCensus() {
+
+    }
+
+    public String getStateName() {
+        return stateName;
+    }
+
+    public void setStateName(String stateName) {
+        this.stateName = stateName;
+    }
+
+    public String getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(String population) {
+        this.population = population;
+    }
+
+    public String getAreaInSqKm() {
+        return areaInSqKm;
+    }
+
+    public void setAreaInSqKm(String areaInSqKm) {
+        this.areaInSqKm = areaInSqKm;
+    }
+
+    public String getDensityPerSqKm() {
+        return densityPerSqKm;
+    }
+    @Override
+    public String toString() {
+        return "stateName='" + stateName + '\'' +
+                ", population='" + population + '\'' +
+                ", areaInSqKm='" + areaInSqKm + '\'' +
+                ", densityPerSqKm='" + densityPerSqKm + '\''
+                +"\n";
+    }
+}
